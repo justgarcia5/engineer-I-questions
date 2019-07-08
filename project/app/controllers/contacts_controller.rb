eebbae18453a18e697c815cac920f1edcebef359
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-07-08 13:17:04 -0700

    first commit

---
+++ project/app/controllers/contacts_controller.rb
@@ -0,0 +1,30 @@
+class ContactsController < ApplicationController
+   # before_action :set_contact, only: [:index ]
+
+   def index
+    @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.create(contact_params)
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:name, :phone, :email)
+  end
+end
---
+++ project/app/models/contact.rb
@@ -0,0 +1,4 @@
+class Contact < ApplicationRecord
+  validates :name, :phone, :email, presence: true
+
+end
