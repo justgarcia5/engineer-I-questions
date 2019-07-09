1cac783f79fe7d593ba49e530f710eb4d045db54
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-07-08 17:34:34 -0700

    css add-ons

--- project/app/models/contact.rb
+++ project/app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
   validates :name, :phone, :email, presence: true
+  validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "xxx-xxx-xxxx format required" }
 
 end
