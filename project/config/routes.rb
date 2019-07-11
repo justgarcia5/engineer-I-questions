c59a80fe3a5927ec5fee82ec8f5b5406e06757d3
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-07-10 21:55:37 -0700

    set up devise for users and no issues

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :contacts
   get '/search' => 'contacts#search', :as => 'search_page'
 
-  root to: 'contacts#index'
+  root to: 'home#index'
 end
