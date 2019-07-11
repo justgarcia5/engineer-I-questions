f9a9d4f48719779e1beda3d6ec2ec1dbc4e2604a
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-07-11 10:46:22 -0700

    changes to user sign up field

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -35,5 +35,4 @@ ActiveRecord::Schema.define(version: 2019_07_11_054723) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "contacts", "users"
 end
