1b2a11790b7a0cd0fd53912609778dc1f6a8cb71
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-07-13 23:56:55 -0700

    updated and added tests/all passing

--- project/spec/features/user_spec.rb
+++ project/spec/features/user_spec.rb
@@ -7,8 +7,20 @@ feature 'User can see a list of contacts' do
   let(:user) { create_logged_in_user }
   scenario 'clicks contact list' do
     visit contacts_path(user)
-    click_link "#{user.email}'s Contacts"
+    click_link "#{user.name}'s Contacts"
     expect(page).to have_content('Contacts')
+    expect(page).to have_link('Edit')
+  end
+end
+feature 'User can edit profile' do
+  let(:user) { create_logged_in_user }
+  scenario 'clicks edit button' do
+    visit contacts_path(user)
+    click_link "Edit profile"
+    expect(page).to have_content('Edit User')
+    expect(page).to have_button('Update')
+    expect(page).to have_button('Delete')
+    expect(page).to have_link('Back')
   end
 end
 feature 'User can log out' do
