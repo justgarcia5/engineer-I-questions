6d3cae54ae87a5b246457b2986448852def9221a
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-07-15 21:16:03 -0700

    removed home view and controller

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable
   validates :name, presence: true
 
-  has_many :contacts
+  has_many :contacts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
